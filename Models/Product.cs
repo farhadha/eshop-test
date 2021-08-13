30f343d8dbff5fd65b5f55f6d8ca6a3140fbab38
Farhadha <faha1199@gmail.com>
2021-08-14 01:08:45 +0430

    add documetation

--- Models/Product.cs
+++ Models/Product.cs
@@ -5,14 +5,29 @@ using System.Threading.Tasks;
 
 namespace Models
 {
+    /// <summary>
+    /// Product
+    /// </summary>
     public class Product
     {
+        /// <summary>
+        /// Product Id
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// Product Name
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Product Category
+        /// </summary>
         public string Category { get; set; }
 
+        /// <summary>
+        /// Product Price
+        /// </summary>
         public int Price { get; set; }
     }
 }
